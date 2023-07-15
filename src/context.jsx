9f57ca42502b8709781a70212b75928e204342da
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-07-15 18:10:45 +0530

    global context setup

---
+++ src/context.jsx
@@ -0,0 +1,12 @@
+import { useContext, useReducer, useEffect, createContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const greeting = "hello";
+    return <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
